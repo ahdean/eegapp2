4185301ae0648c0c70fdbb1f01944960bbb998a4
ahdean <alex@heyzap.com>
2014-12-01 15:59:00 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 # ui.R
 shinyUI(fluidPage(
-  titlePanel("EEGapp - an exploratory tool for analysis of intracraniel spontaneous seizure data"),
+  titlePanel("EEGapp - an exploratory tool for analysis of spontaneous seizure data"),
   
   sidebarLayout(
     sidebarPanel(
